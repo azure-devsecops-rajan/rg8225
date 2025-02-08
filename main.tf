ab740f6a166e15df3f6dae03eda59bf491a8c7bf
Name <you@example.com>
2025-02-08 14:51:54 +0530

    destroy rg5

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "azurerm" {
   subscription_id = "3ba70bac-d8e7-4f2b-8e18-7fa19d2fb0f1"
   features {}
 }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg"
+    storage_account_name = "redstora2gact12"
+    container_name       = "cntr"
+    key                  = "terraform.tfstate"
+
+  }
+}
 resource "azurerm_resource_group" "rg" {
   name     = "rg-terraform1"
   location = "East US"
